d6f2ebc86c0a05bedc48bd4b761a49b2afe2b3a6
Easin Webpro <easinislam299@gmail.com>
2023-05-18 17:33:49 +0600

    🚧 working on login register handler func

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,12 +1,44 @@
 'use client';
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
 
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
 
     const [variant, setVariant] = useState<Variant>('LOGIN');
+    const [isLoading, setIsLoading] = useState(false);
+    const toggleVariant = useCallback(() => {
+        if (variant === 'LOGIN') {
+            setVariant('REGISTER');
+        } else {
+            setVariant('LOGIN');
+        }
+    }, [variant]);
+
+    const { register, handleSubmit, formState: { errors } } = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: '',
+        }
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true);
+
+        if (variant == 'REGISTER') {
+            //call axois register
+        }
+        if (variant == 'LOGIN') {
+            //Call NextAuth lib
+        }
+    }
+
+    const socialAction = (action: string) => {
+        //nextAuth Social login
+    }
 
     return (
         <h4>Hello Auth Form</h4>
